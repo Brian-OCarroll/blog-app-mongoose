3c465180fddc8de9f9ed53b3f80a680cc71c62da
Brian O'Carroll <bocarroll36@gmail.com>
2018-09-24 14:45:13 -0700

    d

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 "use strict";
 //change url to something for my database
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                      'mongodb://bocarroll36:cinnoman125@ds047095.mlab.com:47095/blog-app';
+                      'mongodb://localhost/blogPosts';
 exports.PORT = process.env.PORT || 8080;
